5726d5ded5abecea364ee31fa885aa24df701c87
Ian Kent <email@iankent.co.uk>
2017-04-25 22:04:06 +0100

    refactor

--- data/model/user.go
+++ data/model/user.go
@@ -45,24 +45,3 @@ type Role struct {
 // Permission ...
 type Permission struct {
 }
-
-// Collection ...
-type Collection struct {
-	ID              string        `bson:"_id,omitempty"`
-	Name            string        `bson:"name"`
-	CollectionOwner string        `bson:"collection_owner"`
-	PendingDeletes  []interface{} `bson:"pending_deletes"`
-	PublishDate     *time.Time    `bson:"publish_date"`
-	ReleaseURI      string        `bson:"release_uri"`
-	Teams           []interface{} `bson:"teams"`
-	Type            string        `bson:"type"`
-	Published       bool          `bson:"published"`
-}
-
-// CollectionEvent ...
-type CollectionEvent struct {
-	ID           string    `bson:"_id,omitempty"`
-	Email        string    `bson:"email"`
-	CollectionID string    `bson:"collection_id"`
-	Created      time.Time `bson:"created"`
-}
